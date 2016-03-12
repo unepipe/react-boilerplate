f4c34c68733b19ad1a257909075204cae0c7b662
Brent <brent.pirruccello@gmail.com>
2016-03-12 04:00:40 -0800

    Added react
    added preliminary react stuff (Hello world)

---
+++ src/client/app/index.jsx
@@ -0,0 +1,1 @@
+﻿import React from 'react';import {render} from 'react-dom';class App extends React.Component {  render () {    return (      <div>        <p>Hello React!</p>      </div>    );  }}render(<App/>, document.getElementById('app'));
