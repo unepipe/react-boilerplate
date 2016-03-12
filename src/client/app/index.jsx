7bb48ddff70f6928d47be3603ecc045e615f314d
Brent <brent.pirruccello@gmail.com>
2016-03-12 04:37:32 -0800

    Added counter button

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,1 @@
-﻿import React from 'react';import {render} from 'react-dom';class App extends React.Component {  render () {    return (      <div>        <p>Hello React!</p>      </div>    );  }}render(<App/>, document.getElementById('app'));
+﻿import React from 'react';import {render} from 'react-dom';import AwesomeComponent from './AwesomeComponent.jsx';class App extends React.Component {  render () {    return (      <div>        <p>Hello React!</p>        <AwesomeComponent />      </div>    );  }}render(<App/>, document.getElementById('app'));
